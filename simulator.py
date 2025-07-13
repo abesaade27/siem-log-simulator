4a835607c5e4c1d76476cce9f8b69fdcbd0f765d
abesaade <saadbinarif789@gmail.com>
2025-07-14 00:48:18 +0500

    Added a start and stop button for simulation

--- simulator.py
+++ simulator.py
@@ -2,6 +2,9 @@ import yaml
 import random
 from generators.ssh_generator import generate_ssh_log
 from generators.utils import wait_interval
+import threading
+
+stop_event = threading.Event()
 
 def load_config(path='config.yaml'):
     with open(path, 'r') as file:
@@ -17,20 +20,34 @@ def load_config(path='config.yaml'):
         print(f"Failed to load config: {e}")
         return None
 
-
-def simulate_ssh_logs(config):
+def simulate_ssh_logs(config, stop_event):
     total = config.get('total_events', 50)
     rate = config.get('rate_per_minute', 10)
     output_file = config.get('output_file', 'logs/ssh_simulation.log')
     success_rate = config.get('success_rate', 0.7)
 
+    print("[Simulator] Starting Simulation...")
     with open(output_file, 'w') as f:
         for _ in range(total):
+            if stop_event.is_set():
+                print("[Simulator] Simulation stopped.")
+                break
+
             success = random.random() < success_rate
             log = generate_ssh_log(success)
             print(log)
             f.write(log + '\n')
             wait_interval(rate)
+
+
+def stop_simulation():
+    stop_event.set()
+    print("Simulation stopped.")
+
+def reset_simulation():
+    global stop_event
+    stop_event = threading.Event()
+    print("Simulation reset.")
 
 if __name__ == '__main__':
     config = load_config()
